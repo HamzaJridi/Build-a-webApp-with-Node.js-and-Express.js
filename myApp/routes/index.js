9deb450ba6847e1d5db221c07f71cf70d2aab268
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-22 22:28:24 +0100

    add speakers route

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -14,4 +14,17 @@ router.get('/', function(req, res) {
   });
 });
 
+/* GET speakers page.*/
+router.get('/speakers', function(req, res) {
+    var  myArtwork = [];
+    appdata.speakers.forEach(function(item) {
+        myArtwork = myArtwork.concat(item.artwork);
+    });
+    res.render('speakers', {
+        title: 'Speakers',
+        artwork : myArtwork
+    });
+});
+
+
 module.exports = router;
